30ebace8a3dbb5bc2b90493bee1cb0fa468c738b
Leo <algo2000@naver.com>
2021-06-05 11:50:49 +0900

    up: 메인 레이아웃 스타일 수정

--- MY_APP/renderer/layouts/MainLayout/style.ts
+++ MY_APP/renderer/layouts/MainLayout/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { ListItem } from "@material-ui/core";
 
 const MenuListItem = styled(ListItem)`
-  padding: 10px 30px 10px 15px;
+  padding: 10px 50px 10px 15px;
   * {
     margin: 0px 5px;
   }
