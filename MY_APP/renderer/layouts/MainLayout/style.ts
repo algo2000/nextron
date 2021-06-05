54f1bd26e90ff904ed04e2b66782f8ebbf4ded24
Leo <algo2000@naver.com>
2021-06-05 11:19:59 +0900

    add: MainLayout 스타일 추가

---
+++ MY_APP/renderer/layouts/MainLayout/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+import { ListItem } from "@material-ui/core";
+
+const MenuListItem = styled(ListItem)`
+  padding: 10px 30px 10px 15px;
+  * {
+    margin: 0px 5px;
+  }
+`;
+
+const style = {
+  MenuListItem,
+};
+
+export default style;
